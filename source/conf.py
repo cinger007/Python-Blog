d1d4d03ee613be679a6ccda45d164282b3690ce9
cinger007 <isabelincoln@gmail.com>
2019-12-03 17:08:55 +0800

    for tables

--- source/conf.py
+++ source/conf.py
@@ -36,7 +36,8 @@ source_suffix = ['.rst', '.md']
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['chinese_search']
+extensions = ['chinese_search',
+'sphinx-markdown-tables']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
